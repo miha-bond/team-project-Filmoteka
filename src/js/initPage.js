14d037c76c7901e9b2dc12a08371506bf4bd7edb
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2022-10-30 20:06:17 +0200

    Добавив можливість зберігати стартову сторінку в localStorage

--- src/js/initPage.js
+++ src/js/initPage.js
@@ -2,7 +2,7 @@ import createMarkup from './galleryMarkup';
 import ThemoviedbAPI from './themoviedbAPI';
 import './modalCard';
 import localStorageRecharge from './storage-proceccing';
-
+import { load, save } from './storage';
 
 const API = new ThemoviedbAPI();
 initPage();
@@ -11,4 +11,5 @@ export default async function initPage() {
   const popularMovies = await API.getPopularMovies();
   localStorageRecharge(popularMovies);
   createMarkup(popularMovies);
+  save('page', popularMovies);
 }
