6f6941a82216c21a92db534b35111531d6f0aa61
MaryKotliar <glinskayam@ukr.net>
2022-10-31 09:42:09 +0200

    pagination good looking
    Пагинация c кодом Леси

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,11 +3,113 @@ import { refs } from './refs';
 import createMarkup from './galleryMarkup';
 const API = new ThemoviedbAPI();
 
+export function createPaginationLayout({ page, total_pages }) {
+  const dataSet = createArr(page, total_pages);
+  renderPagination(dataSet, page, total_pages);
 
-let currentPage = API.page;
-let perPage;
+}
 
-const LS_CURRENT_PAGE_KEY = currentPage;
+function createArr(start, end) {
+  const arr = [];
+
+  for (let i = start - 2; i <= start + 2; i += 1) {
+    if (i >= 1 && i <= end) {
+      arr.push(i);
+    }
+  }
+
+  if (arr[0] === 2) arr.unshift(1);
+  if (arr[0] > 3) arr.unshift(1, 0);
+
+  if (arr[arr.length - 1] < end) arr.push(0, end);
+  if (arr[arr.length - 1] === end - 2) arr.push(end - 1);
+  if (arr[arr.length - 1] === end - 3) arr.push(end - 2);
+
+  return arr;
+}
+
+function renderPagination(arr, currentPage, lastPage) {
+  const markup = arr.map(index => {
+    if (index === 0)
+      return `<li class="pagination__item">
+            <div class="???">...</div>
+          </li>`;
+    if (index === 1)
+      return `
+        <li class = "pagination__item">
+          <button class = "${
+            index === currentPage ? 'pagination__item--active' : ''
+          }" type = "button" id = '${index}'>1</button>
+        </li>
+      `;
+    if (index === lastPage)
+      return `
+        <li class="pagination__item">
+          <button class="${
+            index === currentPage ? 'pagination__item--active' : ''
+          }" type = "button" id = '${index}'>
+            ${lastPage}
+          </button>
+        </li>
+      `;
+
+    return `
+        <li class="pagination__item">
+          <button class="${
+            index === currentPage ? 'pagination__item--active' : ''
+          }" type = "button" id = '${index}'>
+            ${index}
+          </button>
+        </li>
+      `;
+  });
+
+  // markup.unshift`(<li class="pagination__item">${refs.prevBtn.outerHTML}</li>)`;
+  // markup.push`(<li class="pagination__item">${refs.nextBtn.outerHTML}</li>)`;
+
+  refs.paginationList.innerHTML = '';
+  refs.paginationList.insertAdjacentHTML('beforeend', markup.join(' '));
+
+  createPaginationBtn();
+}
+
+function createPaginationBtn() {
+  const paginationItem = document.querySelectorAll('.pagination__item button');
+  paginationItem.forEach(item => {
+    item.addEventListener('click', e => {
+      if (e.currentTarget.id === 'prevBtn') return prevPage();
+      if (e.currentTarget.id === 'nextBtn') return nextPage();
+      API.page = e.target.id;
+      main();
+    });
+  });
+}
+
+async function main() {
+  const popularMovies = await API.getPopularMovies();
+  refs.galleryItem.innerHTML = '';
+  createMarkup(popularMovies);
+  createPaginationLayout(popularMovies);
+}
+
+async function prevPage() {
+  API.decrementPage();
+  main();
+}
+
+async function nextPage() {
+  API.incrementPage();
+  main();
+}
+
+refs.prevBtn.addEventListener('click', prevPage);
+
+refs.nextBtn.addEventListener('click', nextPage);
+
+// let currentPage = API.page;
+// let perPage;
+
+// const LS_CURRENT_PAGE_KEY = currentPage;
 
 // export function createPaginationLayout({ page, total_pages }) {
 //   const dataSet = createArr(page, total_pages);
@@ -57,63 +159,63 @@ const LS_CURRENT_PAGE_KEY = currentPage;
 //   createPaginationBtn(currentPage);
 // }
 
-export function createPagination({ results }) {
-  //тут приходит массив фильмов, в зависимости от длины массива - рендарится кол-во кнопок
-  for (let i = 0; i < results.length; i += 1) {
-    const liEl = createPaginationBtn(i + 1);
-    refs.paginationList.appendChild(liEl);
-  }
-}
+// export function createPagination({ results }) {
+//   //тут приходит массив фильмов, в зависимости от длины массива - рендарится кол-во кнопок
+//   for (let i = 0; i < results.length; i += 1) {
+//     const liEl = createPaginationBtn(i + 1);
+//     refs.paginationList.appendChild(liEl);
+//   }
+// }
 
-function createPaginationBtn(page) {
-  const liEl = document.createElement('li');
-  liEl.classList.add('pagination__item');
-  liEl.textContent = page;
+// function createPaginationBtn(page) {
+//   const liEl = document.createElement('li');
+//   liEl.classList.add('pagination__item');
+//   liEl.textContent = page;
 
-  if (currentPage === page) liEl.classList.add('pagination__item--active');
+//   if (currentPage === page) liEl.classList.add('pagination__item--active');
 
-  liEl.addEventListener('click', () => {
-    API.page = page;
-    currentPage = API.page;
-    perPage = API.perPage;
-    main();
+//   liEl.addEventListener('click', () => {
+//     API.page = page;
+//     currentPage = API.page;
+//     perPage = API.perPage;
+//     main();
 
-    async function main() {
-      const popularMovies = await API.getPopularMovies();
-      refs.galleryItem.innerHTML = '';
-      createMarkup(popularMovies);
-    }
+//     async function main() {
+//       const popularMovies = await API.getPopularMovies();
+//       refs.galleryItem.innerHTML = '';
+//       createMarkup(popularMovies);
+//     }
 
-    let currentItemLi = document.querySelector('li.pagination__item--active');
-    currentItemLi.classList.remove('pagination__item--active');
+//     let currentItemLi = document.querySelector('li.pagination__item--active');
+//     currentItemLi.classList.remove('pagination__item--active');
 
-    liEl.classList.add('pagination__item--active');
+//     liEl.classList.add('pagination__item--active');
     
-  });
+//   });
 
-  return liEl;
-}
+//   return liEl;
+// }
 
-refs.prevBtn.addEventListener('click', prevPage);
+// refs.prevBtn.addEventListener('click', prevPage);
 
-refs.nextBtn.addEventListener('click', nextPage);
-async function prevPage() {
-  refs.galleryItem.innerHTML ="";
+// refs.nextBtn.addEventListener('click', nextPage);
+// async function prevPage() {
+//   refs.galleryItem.innerHTML ="";
   
-    API.decrementPage();
-    const data = await API.getPopularMovies();
-    createMarkup(data);
+//     API.decrementPage();
+//     const data = await API.getPopularMovies();
+//     createMarkup(data);
   
   
-  // if(currentPage === 1) {
-  //   refs.prevBtn.classList.add('is-hidden')
-  // }
-}
-async function nextPage() {
-  refs.galleryItem.innerHTML ="";
-  API.incrementPage();
-  const data = await API.getPopularMovies();
-  createMarkup(data);
+//   // if(currentPage === 1) {
+//   //   refs.prevBtn.classList.add('is-hidden')
+//   // }
+// }
+// async function nextPage() {
+//   refs.galleryItem.innerHTML ="";
+//   API.incrementPage();
+//   const data = await API.getPopularMovies();
+//   createMarkup(data);
 
 
   
@@ -164,7 +266,7 @@ async function nextPage() {
   //   console.log(data.total_pages);
   //   refs.nextBtn.classList.add('is-hidden')
   // }
-}
+// }
 
 // export function createPagination({page, total_pages}) {
 //     let liTag = '';
@@ -312,21 +414,21 @@ async function nextPage() {
 //   // renderPagination(dataSet, page, total_pages);
 // }
 
-function createArr(start, end) {
-  const arr = [];
+// function createArr(start, end) {
+//   const arr = [];
 
-  for (let i = start - 2; i <= start + 2; i += 1) {
-    if (i >= 1 && i <= end) {
-      arr.push(i);
-    }
-  }
+//   for (let i = start - 2; i <= start + 2; i += 1) {
+//     if (i >= 1 && i <= end) {
+//       arr.push(i);
+//     }
+//   }
 
-  if (arr[0] === 2) arr.unshift(1);
-  if (arr[0] > 3) arr.unshift(1, 0);
+//   if (arr[0] === 2) arr.unshift(1);
+//   if (arr[0] > 3) arr.unshift(1, 0);
 
-  if (arr[arr.length - 1] < end) arr.push(0, end);
-  if (arr[arr.length - 1] === end - 2) arr.push(end - 1);
-  if (arr[arr.length - 1] === end - 3) arr.push(end - 2);
+//   if (arr[arr.length - 1] < end) arr.push(0, end);
+//   if (arr[arr.length - 1] === end - 2) arr.push(end - 1);
+//   if (arr[arr.length - 1] === end - 3) arr.push(end - 2);
 
-  return arr;
-}
+//   return arr;
+// }
