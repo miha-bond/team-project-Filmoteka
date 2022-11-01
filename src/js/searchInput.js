e6069425851cb9d93406b834ba0c171df45b8c93
Miha Bondarenko <limon18@ukr.net>
2022-11-01 20:08:55 +0200

    add body font-family: Roboto;

--- src/js/searchInput.js
+++ src/js/searchInput.js
@@ -1,10 +1,8 @@
 import ThemoviedbAPI from './themoviedbAPI';
 import createMarkup from './galleryMarkup';
 import { refs } from './refs';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const filmsApi = new ThemoviedbAPI();
 
@@ -15,26 +13,25 @@ async function onFormSubmit(evt) {
   try {
     filmsApi.query = evt.currentTarget.elements.searchQuery.value.trim();
     if (filmsApi.query === '') return;
-     
-      const films = await filmsApi.getMovieByName();
-       
-      Loading.standard({
+
+    const films = await filmsApi.getMovieByName();
+
+    Loading.standard({
       svgSize: '150px',
-      });
+    });
     console.log(filmsApi);
     refs.galleryItem.innerHTML = '';
-      createMarkup(films);
-      Notify.success(`We found ${films.total_results} movies for your query`);
+    createMarkup(films);
+    Notify.success(`We found ${films.total_results} movies for your query`);
     searchFormRef.reset();
   } catch (error) {
-      console.log(error);
+    console.log(error);
     // if (films.total_results.length === 0) {
     //   Notify.failure(
     //     'Sorry, there are no movies matching your search query. Please try again.'
     //   );
-      
+
     // }
-    
   }
-    Loading.remove();
+  Loading.remove();
 }
