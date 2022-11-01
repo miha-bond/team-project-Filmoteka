dcf646a82bc3042a6ca178b3dc1973fff46f84ef
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2022-11-01 18:06:54 +0200

    team-modal

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './js/modalCard';
 import './js/modalfilm';
 import './js/renderModal';
 import './js/searchInput';
-
+import './js/team-modal';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
--- src/js/ganresId.js
+++ src/js/ganresId.js
@@ -75,4 +75,4 @@ export const genres = [
       "id": 37,
       "name": "Western"
     },
-  ]
+]
