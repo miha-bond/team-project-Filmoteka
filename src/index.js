5f9aaef11b444cec0183535cb9dc4a8982b00a3c
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2022-11-03 00:11:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import './js/filter';
 import './js/initPage';
 import './js/modalCard';
 import './js/renderModal';
-// import './js/colorButton';
 import './js/searchInput';
 import './js/team-modal';
+import './js/videotube';
