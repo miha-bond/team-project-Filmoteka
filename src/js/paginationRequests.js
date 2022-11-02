ee0d5c0735e96eaaafadb833030ffd52465b6302
MaryKotliar <glinskayam@ukr.net>
2022-11-02 21:00:16 +0200

    pagination without genres and styles

---
+++ src/js/paginationRequests.js
@@ -0,0 +1,23 @@
+import ThemoviedbAPI from './themoviedbAPI';
+import { refs } from './refs';
+import createMarkup from './galleryMarkup';
+import { createPaginationLayout } from './paginationMarkup';
+
+const API = new ThemoviedbAPI();
+
+export async function renderPaginationOnSearch(page, query) {
+  API.query = query;
+  API.page = page;
+  const movie = await API.getMovieByName();
+  refs.galleryItem.innerHTML = '';
+  createMarkup(movie);
+  createPaginationLayout(movie, query);
+}
+
+export async function renderPaginationOnPopular(page) {
+  API.page = page;
+  const movie = await API.getPopularMovies();
+  refs.galleryItem.innerHTML = '';
+  createMarkup(movie);
+  createPaginationLayout(movie);
+}
