06e96a84cee5e127ce4e19a2974cab8a4688953f
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2022-11-03 22:58:46 +0200

    add options-notiflix

---
+++ src/js/options-notiflix.js
@@ -0,0 +1,28 @@
+export const options = {
+  width: '393px',
+  position: 'center-top',
+  distance: '',
+  fontFamily: 'Roboto',
+  fontSize: '14px',
+  timeout: 2000,
+  useIcon: false,
+  cssAnimation: false,
+  showOnlyTheLastOne: true,
+
+  success: {
+    background: 'transparent',
+    textColor: '#FF6B08',
+  },
+  failure: {
+    background: 'transparent',
+    textColor: '#FF001B',
+  },
+  warning: {
+    background: 'transparent',
+    textColor: '#E9DF02',
+  },
+  info: {
+    background: 'transparent',
+    textColor: '#08C4FF',
+  },
+};
