64e1302e206e6324b91ce27ad503dc109bb5118e
serhii_dem <realsergeiii0@gmail.com>
2022-11-03 12:21:32 +0300

    fixed library buttons & other...

--- src/js/modalCard.js
+++ src/js/modalCard.js
@@ -1,49 +1,74 @@
-// import ThemoviedbAPI from './themoviedbAPI';
 import { renderModal } from './renderModal';
 
 import { load, save } from './storage';
 import { refs } from './refs';
 
-// const API = new ThemoviedbAPI();
+import {initStorage, toggleToWatched,
+toggleToQueue } from "./storage-proceccing";
+import initPage from './initPage';
 
-if (refs.renderModal) {
-  refs.renderModal.addEventListener('click', showCard);
-}
+export const buttonIsOnClassName = 'is-on';
+export function showCard(e) {
+  e.preventDefault();
+   if(e.target.nodeName === "IMG" || e.target.nodeName === "H2" || e.target.nodeName === "P" ) {
 
-// window.addEventListener('keydown', closeModalHandler);
+ const parentContainer = sortedTargetElement(e);
+  const selectedCardId = Number(parentContainer.id);
+const getCurrentPage = load('currentPage');
+  const getCardData = getCurrentPage.find(element => element.id === selectedCardId);
+renderModal(getCardData);
 
-function closeModalHandler(e) {
+const addTWtchBtn = document.querySelector('#watched');
+const addToQueBtn = document.querySelector('#queue');
+ const STORAGE_LIBR_KEY1 = addTWtchBtn.id; 
+const STORAGE_LIBR_KEY2 = addToQueBtn.id;
+   initStorage(STORAGE_LIBR_KEY1, parentContainer.id, document.querySelector('#watched'));
+   initStorage(STORAGE_LIBR_KEY2, parentContainer.id, document.querySelector('#queue'));
+  
+
+   
+     refs.backdrop.classList.remove('is-hidden');
+     refs.body.classList.add('no-scroll');
+     document.querySelector('[data-modal-close]').addEventListener('click', onCloseBtn);
+     addTWtchBtn.addEventListener('click', toggleToWatched);
+     addToQueBtn.addEventListener('click', toggleToQueue);
+     window.addEventListener('keydown', closeModalHandler);
+
+    
+   }
+  }
+      function onCloseBtn(e) {
+          if (document.title === 'Page Queue' && document.querySelector('[data-watched]').classList.contains(buttonIsOnClassName) && document.querySelector('#queue').classList.contains(buttonIsOnClassName)|| document.title === 'Page Queue' && document.querySelector('[data-queue]').classList.contains(buttonIsOnClassName) && document.querySelector('#watched').classList.contains(buttonIsOnClassName) || document.title === 'Page Queue' && document.querySelector('[data-watched]').classList.contains(buttonIsOnClassName) && ! document.querySelector('#watched').classList.contains(buttonIsOnClassName) || document.title === 'Page Queue' && document.querySelector('[data-queue]').classList.contains(buttonIsOnClassName) && ! document.querySelector('#queue').classList.contains(buttonIsOnClassName)){
+            document.getElementById(e.target.parentNode.parentNode.id).remove();
+
+           }
+         refs.body.classList.remove('no-scroll');
+         refs.backdrop.classList.add('is-hidden');
+         document.querySelector('#watched').removeEventListener('click', toggleToWatched);
+         document.querySelector('#queue').removeEventListener('click', toggleToQueue);
+         document.querySelector('[data-modal-close]').removeEventListener('click', onCloseBtn);
+       }
+
+      
+function sortedTargetElement (e) {
+  let element = null;
+  if (e.target.nodeName === 'IMG') {
+    element = e.target.parentNode.parentNode;
+  } else if (e.target.nodeName === 'P' || e.target.nodeName === 'H2') {
+    element = e.target.parentNode.parentNode.parentNode;
+  } else {
+    return
+  }
+  return element;
+  }
+ 
+
+ function closeModalHandler(e) {
   if (e.code === 'Escape') {
     refs.backdrop.classList.add('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    refs.body.classList.add('no-scroll');
     window.removeEventListener('keydown', closeModalHandler);
+  
   }
 }
 
-function showCard(e) {
-  e.preventDefault();
-  window.addEventListener('keydown', closeModalHandler);
-
-  if (e.target.nodeName === 'DIV') {
-    return;
-  }
-  const selectedCardId = Number(e.target.id);
-  const getCurrentPage = load('currentPage');
-
-  getCurrentPage.forEach(element => {
-    if (selectedCardId === element.id) {
-      renderModal(element);
-      let closeModal = document.querySelector('[data-modal-close]');
-      closeModal.addEventListener('click', onCloseBtn);
-
-      function onCloseBtn() {
-        window.removeEventListener('keydown', closeModalHandler);
-        refs.body.classList.toggle('no-scroll');
-        refs.backdrop.classList.add('is-hidden');
-      }
-    }
-  });
-
-  refs.backdrop.classList.remove('is-hidden');
-  refs.body.classList.toggle('no-scroll');
-}
