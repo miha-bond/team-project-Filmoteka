b16893dd75c2404f92957716f8ab2ad963a8a127
Miha Bondarenko <limon18@ukr.net>
2022-11-04 15:05:36 +0200

    changes color notiflix

--- src/js/options-notiflix.js
+++ src/js/options-notiflix.js
@@ -1,21 +1,21 @@
 export const options = {
-  width: 'auto',
+  width: '250px',
   position: 'center-top',
   distance: '',
   fontFamily: 'Roboto',
-  fontSize: '14px',
+  fontSize: '12px',
   timeout: 3500,
   useIcon: true,
   cssAnimation: false,
   showOnlyTheLastOne: true,
 
   success: {
-    background: '#17bb01',
-    textColor: '#000000',
+    background: '#228621',
+    textColor: '#fffff',
   },
   failure: {
-    background: '#d80606',
-    textColor: '#000000',
+    background: '#742431',
+    textColor: '#fffff',
   },
   warning: {
     background: 'red',
