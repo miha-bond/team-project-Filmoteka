a74e436244aa74a1166b1f6693e023237c63aa9b
Miha Bondarenko <limon18@ukr.net>
2022-11-04 11:22:04 +0200

    bug fix searchInput

--- src/js/searchInput.js
+++ src/js/searchInput.js
@@ -1,46 +1,51 @@
 import ThemoviedbAPI from './themoviedbAPI';
-import createMarkup from './galleryMarkup';
+import { createMarkup } from './galleryMarkup';
 import { refs } from './refs';
-import { options } from './options-notiflix';
+import initPage from './initPage';
+import { options } from './options_notiflix';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { renderPaginationOnSearch } from './paginationRequests';
 
 const filmsApi = new ThemoviedbAPI();
 
 refs.searchFormRef.addEventListener('submit', onFormSubmit);
-// loadRef.addEventListener('click', onLoadClick);
 
 async function onFormSubmit(evt) {
   evt.preventDefault();
   const searchQuery = evt.currentTarget.elements.searchQuery.value
     .trim()
     .toLowerCase();
-  if (searchQuery === "") {
+
+  if (searchQuery === '') {
     Notify.failure('Enter a search query!', options);
     return;
   }
+
   try {
-    filmsApi.query = searchQuery 
+    filmsApi.query = searchQuery;
     const searchFilms = await filmsApi.getMovieByName();
     if (searchFilms.total_results !== 0) {
-      Notify.success(`We found ${searchFilms.total_results} movies for your query`, options)
+      Notify.success(
+        `We found ${searchFilms.total_results} movies for your query`,
+        options
+      );
+      refs.galleryItem.innerHTML = '';
+      createMarkup(searchFilms);
+      renderPaginationOnSearch(filmsApi.page, filmsApi.query);
+      refs.searchFormRef.reset();
+    } else if (searchFilms.total_results === 0) {
+      Notify.failure(
+        'Sorry, there are no movies matching your search query. Please try again.',
+        options
+      );
+      initPage();
     }
     Loading.standard({
       svgSize: '150px',
     });
-    
-    console.log(filmsApi);
-    refs.galleryItem.innerHTML = '';
-    createMarkup(searchFilms);
-    searchFormRef.reset();
   } catch (error) {
-    // Notify.failure('Something went wrong! Please retry', options);
-    console.log(error);
-    // if (films.total_results.length === 0) {
-    //   Notify.failure(
-    //     'Sorry, there are no movies matching your search query. Please try again.'
-    //   );
-    // }
+    Notify.failure('Something went wrong! Please retry', options);
   }
   Loading.remove();
 }
